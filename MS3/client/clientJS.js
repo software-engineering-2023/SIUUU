579331ba9537663d441ad1f923d2a354b783c0c3
Aly Rezk <121068675+alysantino@users.noreply.github.com>
2023-05-24 18:24:21 +0300

    Add files via upload

--- MS3/client/clientJS.js
+++ MS3/client/clientJS.js
@@ -2,20 +2,44 @@ const signUpButton = document.getElementById('signUp');
 const signIn = document.getElementById('signin-button');
 const signInButton = document.getElementById('signIn');
 const signUp = document.getElementById('signup-button');
-const container = document.getElementById('container');
-if (signInButton) {
+const container = document.getElementById('logInContainer');
+const mainView = document.getElementById('mainView');
+
+const homeButton = document.getElementById('homeButton');
+const home = document.getElementById('home');
+
+const creditCardButton = document.getElementById('creditCardButton');
+const creditCard = document.getElementById('creditCard');
+
+const loan = document.getElementById('loan');
+const loanButton = document.getElementById('loanButton');
+
+const transactionButton = document.getElementById('transactionButton');
+const transaction = document.getElementById('transaction');
+
+const billsButton = document.getElementById('billsButton');
+const bills = document.getElementById('bills');
+
+const transferButton = document.getElementById('transferButton');
+const transfer = document.getElementById('transfer');
+
+const cashBackButton = document.getElementById('cashBackButton');
+const cashBack = document.getElementById('cashBack');
+
+let activeDiv = "home";
+
+
+if (signInButton && signUpButton && container) {
     signUpButton.addEventListener('click', function () {
-        console.log("hello1");
         container.classList.add("right-panel-active");
 
     });
     signInButton.addEventListener('click', function () {
-        console.log("hello1");
         container.classList.remove("right-panel-active");
     })
 };
 
-if (signIn) {
+if (signIn && signUp) {
     signIn.addEventListener('click', function () {
         window.location.href = "clientDashboard.html";
     })
@@ -28,3 +52,60 @@ if (signUp) {
     })
 };
 
+if (homeButton && mainView && home) {
+    homeButton.addEventListener('click', function () {
+        console.log("hello2");
+        setActiveDiv("home");
+    })
+}
+
+if (creditCardButton && mainView && home) {
+    creditCardButton.addEventListener('click', function () {
+        console.log("hello3");
+        setActiveDiv("creditCard");
+    })
+}
+
+if (loanButton && mainView && home) {
+    loanButton.addEventListener('click', function () {
+        console.log("hello4");
+        setActiveDiv("loan");
+    })
+}
+
+if (transactionButton && mainView && home) {
+    transactionButton.addEventListener('click', function () {
+        console.log("hello5");
+        setActiveDiv("transaction");
+    })
+}
+
+if (billsButton && mainView && home) {
+    billsButton.addEventListener('click', function () {
+        console.log("hello6");
+        setActiveDiv("bills");
+    })
+}
+
+if (transferButton && mainView && home) {
+    transferButton.addEventListener('click', function () {
+        console.log("hello7");
+        setActiveDiv("transfer");
+    })
+}
+
+if (cashBackButton && mainView && home) {
+    cashBackButton.addEventListener('click', function () {
+        console.log("hello8");
+        setActiveDiv("cashBack");
+    })
+}
+
+
+function setActiveDiv(divId) {
+    if (divId !== activeDiv) {
+        document.getElementById(activeDiv).style.display = "none";
+        document.getElementById(divId).style.display = "block";
+        activeDiv = divId; // Update the active div variable
+    }
+}
