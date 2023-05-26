569103795453e55b97b1dd872775bb0fa007f83f
rodaina03 <rodaina.abdelbaky@student.guc.edu.eg>
2023-05-26 04:31:52 +0300

    transfer updates

--- MS3/client/clientJS.js
+++ MS3/client/clientJS.js
@@ -120,12 +120,21 @@ function setActiveDiv(divId) {
         activeDiv = divId; // Update the active div variable
     }
 }
-let popup1=document.getElementById("popup1"); //notification popup
+let popup1=document.getElementById("popup1"); //loan popup
 
         function openPopup1(){
             popup1.classList.add("open-popup");
         }
         function closePopup1(){
             popup1.classList.remove("open-popup");
+        }
+
+let popup2=document.getElementById("popup2"); //transfer popup
+
+        function openPopup2(){
+            popup2.classList.add("open-popup");
+        }
+        function closePopup2(){
+            popup2.classList.remove("open-popup");
         }
         
